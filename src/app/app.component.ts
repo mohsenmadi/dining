583c048d72e55490090d35cd35a591b04206b58f
Mohsen <mohsenmadi@gmail.com>
2024-02-01 22:42:58 -0500

    material installed and tried

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, RouterOutlet, MatInputModule, FormsModule,
+    MatFormFieldModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
