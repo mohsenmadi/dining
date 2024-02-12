c2c77964c532d20541f28c8e98fd861296e6f737
Mohsen <mohsenmadi@gmail.com>
2024-02-12 11:50:36 -0500

    sorting done

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
 import {TaskService} from "./task.service";
-import {catchError, concatMap, filter, of, tap} from "rxjs";
+import {catchError, concatMap, filter, of, take, tap} from "rxjs";
 import {NEW_TASK, TaskInterface} from "./task.interface";
 import {openAddUpdateTaskDialog} from "../task-add-update/task-add-update.component";
 import {
@@ -11,12 +11,13 @@ import {
   MatHeaderCell, MatHeaderCellDef,
   MatHeaderRow,
   MatHeaderRowDef,
-  MatRow, MatRowDef, MatTable
+  MatRow, MatRowDef, MatTable, MatTableDataSource, MatTableModule
 } from "@angular/material/table";
 import {MatIcon} from "@angular/material/icon";
 import {MatFabButton, MatIconButton} from "@angular/material/button";
 import {MatTooltip} from "@angular/material/tooltip";
 import {DatePipe} from "@angular/common";
+import {MatSort, MatSortModule} from "@angular/material/sort";
 
 @Component({
   selector: 'app-tasks',
@@ -36,17 +37,26 @@ import {DatePipe} from "@angular/common";
     MatFabButton,
     MatTooltip,
     MatHeaderCellDef,
-    DatePipe
+    DatePipe,
+    MatSort,
+    MatTableModule, MatSortModule
   ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent implements OnInit, AfterViewInit {
 
   displayedColumns: string[] = ['title', 'completed', 'createdAt', 'updatedAt', 'edit', 'delete'];
-  dataSource!: any;
+  // dataSource!: any;
+  dataSource = new MatTableDataSource<TaskInterface>([])
+
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private service: TaskService, private dialog: MatDialog) {
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 
   ngOnInit(): void {
@@ -80,7 +90,13 @@ export class TasksComponent implements OnInit {
   }
 
   private updateDataSource() {
-    this.dataSource = this.service.all();
+    // this.dataSource = this.service.all();
+    // this.dataSource.sort = this.service.all();
+    this.service.all()
+      .pipe(take(1))
+      .subscribe(data =>
+        this.dataSource.data = data
+      )
   }
 
 }
